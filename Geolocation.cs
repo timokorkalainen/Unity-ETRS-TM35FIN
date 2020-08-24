ed2de17bd13f8416e1a8d1dc73c1ef216fd06795
Mikko Arvola <mikko.arvola@ctrlreality.fi>
2020-08-24 16:24:29 +0300

    Added Equals function to make it easier to verify two coordinates are exactly the same

--- Geolocation.cs
+++ Geolocation.cs
@@ -31,4 +31,35 @@ public class Geolocation {
 	override public string ToString() {
 		return longitude + "," + latitude;
 	}
+
+    public override bool Equals(object obj)
+    {
+        return this.Equals(obj as Geolocation);
+    }
+
+    public bool Equals(Geolocation p)
+    {
+        // If parameter is null, return false.
+        if (Object.ReferenceEquals(p, null))
+        {
+            return false;
+        }
+
+        // Optimization for a common success case.
+        if (Object.ReferenceEquals(this, p))
+        {
+            return true;
+        }
+
+        // If run-time types are not exactly the same, return false.
+        if (this.GetType() != p.GetType())
+        {
+            return false;
+        }
+
+        // Return true if the fields match.
+        // Note that the base class is not invoked because it is
+        // System.Object, which defines Equals as reference equality.
+        return (longitude == p.longitude) && (latitude == p.latitude);
+    }
 }
